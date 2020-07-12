d96bc632a2d9fa958553903f58139cfb6ea5afd8
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-12 09:30:10 +0530

    post repository created

---
+++ src/main/java/lk/ac/cmb/ucsc/counselling/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package lk.ac.cmb.ucsc.counselling.repository;
+
+import lk.ac.cmb.ucsc.counselling.model.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post,Long> {
+}
