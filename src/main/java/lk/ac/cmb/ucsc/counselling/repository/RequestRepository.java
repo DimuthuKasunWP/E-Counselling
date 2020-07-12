2d1bc53b11cd6a3a42ff1d08a6d5e1b4240dc6d2
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-12 09:30:38 +0530

    request repository created

---
+++ src/main/java/lk/ac/cmb/ucsc/counselling/repository/RequestRepository.java
@@ -0,0 +1,7 @@
+package lk.ac.cmb.ucsc.counselling.repository;
+
+import lk.ac.cmb.ucsc.counselling.model.Request;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RequestRepository extends CrudRepository<Request,Long> {
+}
